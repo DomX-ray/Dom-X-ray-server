259b7ef878db0b2f30e963b26d8cbf1fdc73926b
csy <csl8804@gmail.com>
2022-12-03 22:52:32 +0900

    refactor: parsing 로직 수정
    for문을 map, promise.all를 이용해 수정

--- routes/controller/index.js
+++ routes/controller/index.js
@@ -9,36 +9,47 @@ const getMain = (req, res, next) => {
 const parseDomTree = async (req, res, next) => {
   try {
     const browser = await puppeteer.launch({
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--use-gl=egl",
-      ],
+      args: ["--no-sandbox", "--disable-setuid-sandbox", "--use-gl=egl"],
     });
     const [page] = await browser.pages();
 
     await page.goto(req.query.searchUrl, {
-      waitUntil: "networkidle2",
+      waitUntil: "networkidle0",
     });
 
     const rootHandle = await page.$("html");
 
     const getDomTree = async (elementHandle) => {
-      const childrenArray = [];
-
       const tagName = await (
         await elementHandle.getProperty("tagName")
       ).jsonValue();
 
+      const textContent = await (
+        await elementHandle.getProperty("textContent")
+      ).jsonValue();
+
+      const id = await (await elementHandle.getProperty("id")).jsonValue();
+
+      const className = await (
+        await elementHandle.getProperty("className")
+      ).jsonValue();
+
       const elementChildren = await elementHandle.$$(":scope > *");
 
-      // todo: map으로 바꾸기(promise.all 이용)
-      for (let i = 0; i < elementChildren.length; i++) {
-        childrenArray.push(await getDomTree(elementChildren[i]));
-      }
+      const childrenCount = elementChildren.length;
+
+      const childrenArray = await Promise.all(
+        elementChildren.map(async (element) => {
+          return await getDomTree(element);
+        })
+      );
 
       return {
         tagName,
+        id,
+        className,
+        childrenCount,
+        textContent,
         ...(childrenArray.length && { children: childrenArray }),
       };
     };
